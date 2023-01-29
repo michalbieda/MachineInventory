23e6c33c1235f06a77ba0cb1857915ff1698b255
Michal Bieda <mikibieda@gmail.com>
2023-01-29 23:22:02 +0100

    Initial commit

---
+++ src/main/java/com/zulusek/App.java
@@ -0,0 +1,15 @@
+package com.zulusek;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        MachineInventory machineInventory = new MachineInventory();
+
+
+
+
+    }
+}
---
+++ src/main/java/com/zulusek/MachineInventory.java
@@ -0,0 +1,9 @@
+package com.zulusek;
+
+public class MachineInventory {
+
+    public int add() {
+        int id = 1;
+        return id;
+    }
+}
